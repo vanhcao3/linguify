86c6946db1a3e597ef9704ebd18c7a788be1173c
Cao Viet Anh <caovietanhbk@gmail.com>
2024-04-17 21:09:36 +0700

    Add auth landing page

--- app/(dashboard)/(routes)/page.tsx
+++ app/(dashboard)/(routes)/page.tsx
@@ -1,5 +1,3 @@
 export default function Home() {
-  return (
-    <p className="text-3xl font-medium text-red-400">Linguify Project Initialized with Nextjs 14 Tailwind Shadcn-ui</p>
-  );
+  return <p className="text-3xl font-medium text-red-400">Trang chủ</p>;
 }
