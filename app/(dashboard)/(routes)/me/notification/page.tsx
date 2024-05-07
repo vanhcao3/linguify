a3812c8fa33091834923212db489ee0dcd9a41ab
TheCoTam <hoangnguyenanhtuyen@gmail.com>
2024-05-07 16:21:43 +0700

    init user notification page

---
+++ app/(dashboard)/(routes)/me/notification/page.tsx
@@ -0,0 +1,85 @@
+import Item from '@/components/Me/NotificationItem';
+import styles from '@/styles/Me/Notification.module.css';
+
+const pseudoNotification = [
+  {
+    image: 'https://files.fullstack.edu.vn/f8-prod/user_photos/276294/6396eb8215dda.jpg',
+    content: 'Chào mừng bạn!!!',
+    createdTime: '25 ngày trước',
+  },
+  {
+    image: 'https://files.fullstack.edu.vn/f8-prod/user_photos/276294/6396eb8215dda.jpg',
+    content: 'Hãy học ngay thôi nào!!!',
+    createdTime: '5 ngày trước',
+  },
+  {
+    image: 'https://files.fullstack.edu.vn/f8-prod/user_photos/276294/6396eb8215dda.jpg',
+    content: 'Hãy học ngay thôi nào!!!',
+    createdTime: '5 ngày trước',
+  },
+  {
+    image: 'https://files.fullstack.edu.vn/f8-prod/user_photos/276294/6396eb8215dda.jpg',
+    content: 'Hãy học ngay thôi nào!!!',
+    createdTime: '5 ngày trước',
+  },
+  {
+    image: 'https://files.fullstack.edu.vn/f8-prod/user_photos/276294/6396eb8215dda.jpg',
+    content: 'Hãy học ngay thôi nào!!!',
+    createdTime: '5 ngày trước',
+  },
+  {
+    image: 'https://files.fullstack.edu.vn/f8-prod/user_photos/276294/6396eb8215dda.jpg',
+    content: 'Hãy học ngay thôi nào!!!',
+    createdTime: '5 ngày trước',
+  },
+  {
+    image: 'https://files.fullstack.edu.vn/f8-prod/user_photos/276294/6396eb8215dda.jpg',
+    content: 'Hãy học ngay thôi nào!!!',
+    createdTime: '5 ngày trước',
+  },
+  {
+    image: 'https://files.fullstack.edu.vn/f8-prod/user_photos/276294/6396eb8215dda.jpg',
+    content: 'Hãy học ngay thôi nào!!!',
+    createdTime: '5 ngày trước',
+  },
+  {
+    image: 'https://files.fullstack.edu.vn/f8-prod/user_photos/276294/6396eb8215dda.jpg',
+    content: 'Hãy học ngay thôi nào!!!',
+    createdTime: '5 ngày trước',
+  },
+  {
+    image: 'https://files.fullstack.edu.vn/f8-prod/user_photos/276294/6396eb8215dda.jpg',
+    content: 'Hãy học ngay thôi nào!!!',
+    createdTime: '5 ngày trước',
+  },
+  {
+    image: 'https://files.fullstack.edu.vn/f8-prod/user_photos/276294/6396eb8215dda.jpg',
+    content: 'Hãy học ngay thôi nào!!!',
+    createdTime: '5 ngày trước',
+  },
+  {
+    image: 'https://files.fullstack.edu.vn/f8-prod/user_photos/276294/6396eb8215dda.jpg',
+    content: 'Hãy học ngay thôi nào!!!',
+    createdTime: '5 ngày trước',
+  },
+];
+
+function Notification() {
+  return (
+    <div className={styles['wrapper']}>
+      <div className={styles['content']}>
+        <div className={styles['header']}>
+          <div className={styles['header-title']}>Thông báo</div>
+          <div className={styles['header-button']}>Đánh dấu là đã đọc</div>
+        </div>
+        <div className={styles['body']}>
+          {pseudoNotification.map((item, index) => {
+            return <Item key={index} data={item} />;
+          })}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Notification;
