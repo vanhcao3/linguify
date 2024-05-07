7ca758d20c7634591885133fce401b420fd4df78
Cao Viet Anh <caovietanhbk@gmail.com>
2024-05-07 11:47:06 +0700

    Create user module: bcrypt password hashing, unique email validation, db pushing

--- actions/register.ts
+++ actions/register.ts
@@ -1,6 +1,7 @@
 'use server';
 import * as z from 'zod';
-
+import { db } from '@/lib/db';
+import bcrypt from 'bcrypt';
 import { RegisterSchema } from '@/schemas';
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
@@ -11,5 +12,26 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
     return { error: 'Invalid fields!' };
   }
 
-  return { success: 'Email sent!' };
+  const { email, password, name } = validatedFields.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const existingUser = await db.user.findUnique({
+    where: {
+      email,
+    },
+  });
+
+  if (existingUser) {
+    return { error: 'Email already taken!' };
+  }
+
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  });
+
+  return { success: 'User created' };
 };
