3c4abc6398a65722c2e498c1de7f1cf3862e022c
Cao Viet Anh <caovietanhbk@gmail.com>
2024-05-07 11:47:28 +0700

    getUserByEmail, getUserById utils

--- actions/register.ts
+++ actions/register.ts
@@ -3,6 +3,7 @@ import * as z from 'zod';
 import { db } from '@/lib/db';
 import bcrypt from 'bcrypt';
 import { RegisterSchema } from '@/schemas';
+import { getUserByEmail } from '@/data/user';
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   //console.log(values);
@@ -15,11 +16,7 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const { email, password, name } = validatedFields.data;
   const hashedPassword = await bcrypt.hash(password, 10);
 
-  const existingUser = await db.user.findUnique({
-    where: {
-      email,
-    },
-  });
+  const existingUser = await getUserByEmail(email);
 
   if (existingUser) {
     return { error: 'Email already taken!' };
@@ -33,5 +30,5 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
     },
   });
 
-  return { success: 'User created' };
+  return { success: 'Userc ' };
 };
