b2dfe97204f5863ceac40b4a3857c3cee7a759e4
Cao Viet Anh <caovietanhbk@gmail.com>
2024-05-08 16:52:50 +0700

    Add jwt token and session token

--- app/something/page.tsx
+++ app/something/page.tsx
@@ -2,8 +2,10 @@ import { auth, signOut } from '@/auth';
 import { Button } from '@/components/ui/button';
 
 const SignOutPage = async () => {
+  const session = await auth();
   return (
-    <div>
+    <div className="flex flex-col items-center justify-center">
+      <div>{JSON.stringify(session)}</div>
       <form
         action={async () => {
           'use server';
--- auth.ts
+++ auth.ts
@@ -2,8 +2,33 @@ import NextAuth from 'next-auth';
 import { PrismaAdapter } from '@auth/prisma-adapter';
 import { db } from './lib/db';
 import authConfig from './auth.config';
+import { getUserById } from './data/user';
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
+  callbacks: {
+    async session({ session, user, token }) {
+      console.log({ sessionToken: token });
+      if (token.sub && session.user) {
+        session.user.id = token.sub;
+      }
+
+      if (token.role && session.user) {
+        session.user.role = token.role;
+      }
+
+      return session;
+    },
+    async jwt({ token }) {
+      if (!token.sub) return token;
+      const existingUser = await getUserById(token.sub);
+
+      if (!existingUser) return token;
+
+      token.role = existingUser.role;
+
+      return token;
+    },
+  },
   adapter: PrismaAdapter(db),
   session: { strategy: 'jwt' },
   ...authConfig,
