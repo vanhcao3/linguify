dd5cd9a44945358c885070f10a867ac1a72a5e48
Cao Viet Anh <caovietanhbk@gmail.com>
2024-05-08 22:37:11 +0700

    Add error login page

--- routes.ts
+++ routes.ts
@@ -1,7 +1,7 @@
 //Array of routes that are available without the need of login
 export const publicRoutes = ['/'];
 //Array of routes specifically for authentification
-export const authRoutes = ['/auth/sign-in', '/auth/sign-up', '/auth'];
+export const authRoutes = ['/auth/sign-in', '/auth/sign-up', '/auth', '/auth/error'];
 //Api routes prefix that should always be accessible to perform sign in sign out functionalities
 export const apiAuthPrefix = '/api/auth';
 //Default path after logging in
