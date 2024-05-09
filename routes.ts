2696273e92e9eb6bfc8f0272fe2e41d04b548d3d
Cao Viet Anh <caovietanhbk@gmail.com>
2024-05-09 09:44:54 +0700

    Add reset password page

--- routes.ts
+++ routes.ts
@@ -1,7 +1,7 @@
 //Array of routes that are available without the need of login
 export const publicRoutes = ['/', '/auth/new-verification'];
 //Array of routes specifically for authentification
-export const authRoutes = ['/auth/sign-in', '/auth/sign-up', '/auth', '/auth/error'];
+export const authRoutes = ['/auth/sign-in', '/auth/sign-up', '/auth', '/auth/error', '/auth/reset'];
 //Api routes prefix that should always be accessible to perform sign in sign out functionalities
 export const apiAuthPrefix = '/api/auth';
 //Default path after logging in
