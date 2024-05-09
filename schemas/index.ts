bf3cac9c893e3231e7745a982411c40ec7e7f1cf
Cao Viet Anh <caovietanhbk@gmail.com>
2024-05-09 22:47:15 +0700

    Add two factor confirmation data model

--- schemas/index.ts
+++ schemas/index.ts
@@ -30,6 +30,7 @@ export const LoginSchema = z.object({
   password: z.string().min(1, {
     message: 'Password is required!',
   }),
+  code: z.optional(z.string()),
 });
 
 export const RegisterSchema = z
