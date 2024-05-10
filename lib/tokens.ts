59e13cdf45dfe02c85069dd740079325eb3ec8cb
Cao Viet Anh <caovietanhbk@gmail.com>
2024-05-10 10:44:29 +0700

    change two factor auth token expiration time to 5 mins

--- lib/tokens.ts
+++ lib/tokens.ts
@@ -7,7 +7,7 @@ import { db } from './db';
 
 export const generateTwoFactorToken = async (email: string) => {
   const token = crypto.randomInt(100_000, 1_000_000).toString();
-  const expires = new Date(new Date().getTime() + 3600 * 1000);
+  const expires = new Date(new Date().getTime() + 5 * 60 * 1000);
   const existingToken = await getTwoFactorTokenByEmail(email);
 
   if (existingToken) {
