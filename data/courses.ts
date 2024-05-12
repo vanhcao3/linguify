12b26323a76d5ea6652ebd823a50dc78d2869544
Cao Viet Anh <caovietanhbk@gmail.com>
2024-05-12 11:30:19 +0700

    Add getCourseById data query

--- data/courses.ts
+++ data/courses.ts
@@ -2,7 +2,16 @@ import { db } from '@/lib/db';
 
 export const getCourseById = async (id: string) => {
   try {
-    const course = await db.course.findUnique({ where: { id } });
+    const course = await db.course.findUnique({
+      where: { id },
+      include: {
+        attachments: {
+          orderBy: {
+            createdAt: 'desc',
+          },
+        },
+      },
+    });
     return course;
   } catch {
     return null;
