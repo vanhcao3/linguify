39656df54f0ff82988618ad33602533d1b4ae9b0
Cao Viet Anh <caovietanhbk@gmail.com>
2024-05-15 23:45:25 +0700

    Feature: chapter publishing. Add Delete chapter functionality

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -4,10 +4,12 @@ import { getChapterById } from '@/data/chapter';
 import Link from 'next/link';
 import { ArrowLeft, Eye, LayoutDashboard, Video } from 'lucide-react';
 import { IconBadge } from '@/components/icon-badge';
+import { Banner } from '@/components/course-banner';
 import { ChapterTitleForm } from '@/components/ui/chapters/chapter-title-form';
 import { ChapterDescriptionForm } from '@/components/ui/chapters/chapter-description-form';
 import { ChapterAccessForm } from '@/components/ui/chapters/chapter-access-form';
 import { ChapterVideoForm } from '@/components/ui/chapters/chapter-video-form';
+import { ChapterPublish } from '@/components/ui/chapters/chapter-publish';
 
 const ChapterIdPage = async ({
   params,
@@ -38,75 +40,89 @@ const ChapterIdPage = async ({
   const totalFields = requiredFields.length;
   const completedFields = requiredFields.filter(Boolean).length;
   const completionText = `(${completedFields}/${totalFields})`;
-
+  const isCompleted = requiredFields.every(Boolean);
   return (
-    <div className="p-6">
-      <div className="flex items-center justify-between">
-        <div className="w-full">
-          <Link
-            href={`/teacher/courses/${params.courseId}`}
-            className="flex items-center text-sm hover:opacity-75 transition mb-6"
-          >
-            <ArrowLeft className="h-4 m-4 mr-2" />
-            Back to edit course
-          </Link>
-          <div className="flex items-center justify-between w-full">
-            <div className="flex flex-col gap-y-2">
-              <h1 className="text-2xl font-medium">
-                Chapter Creation
-              </h1>
-              <span className="text-s text-slate-500">
-                Complete all fields {completionText}
-              </span>
+    <>
+      {!chapter.isPublished && (
+        <Banner
+          variant="warning"
+          label="This chapter is a draft. It will not appear in the chapter list"
+        />
+      )}
+      <div className="p-6">
+        <div className="flex items-center justify-between">
+          <div className="w-full">
+            <Link
+              href={`/teacher/courses/${params.courseId}`}
+              className="flex items-center text-sm hover:opacity-75 transition mb-6"
+            >
+              <ArrowLeft className="h-4 m-4 mr-2" />
+              Back to edit course
+            </Link>
+            <div className="flex items-center justify-between w-full">
+              <div className="flex flex-col gap-y-2">
+                <h1 className="text-2xl font-medium">
+                  Chapter Creation
+                </h1>
+                <span className="text-s text-slate-500">
+                  Complete all fields {completionText}
+                </span>
+              </div>
+              <ChapterPublish
+                disabled={!isCompleted}
+                courseId={params.courseId}
+                chapterId={params.chapterId}
+                isPublished={chapter.isPublished}
+              />
             </div>
           </div>
         </div>
-      </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">
-        <div className="space-y-4">
-          <div>
-            <div className="flex items-center gap-x-2">
-              <IconBadge icon={LayoutDashboard} />
-              <h2>Customizing Chapter</h2>
-            </div>
-            <div>
-              <ChapterTitleForm
-                initialData={chapter}
-                courseId={params.courseId}
-                chapterId={params.chapterId}
-              />
-              <ChapterDescriptionForm
-                initialData={chapter}
-                courseId={params.courseId}
-                chapterId={params.chapterId}
-              />
-            </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">
+          <div className="space-y-4">
             <div>
               <div className="flex items-center gap-x-2">
-                <IconBadge icon={Eye} />
-                <h2 className="text-xl">Access Settings</h2>
+                <IconBadge icon={LayoutDashboard} />
+                <h2>Customizing Chapter</h2>
               </div>
-              <ChapterAccessForm
-                initialData={chapter}
-                courseId={params.courseId}
-                chapterId={params.chapterId}
-              />
+              <div>
+                <ChapterTitleForm
+                  initialData={chapter}
+                  courseId={params.courseId}
+                  chapterId={params.chapterId}
+                />
+                <ChapterDescriptionForm
+                  initialData={chapter}
+                  courseId={params.courseId}
+                  chapterId={params.chapterId}
+                />
+              </div>
+              <div>
+                <div className="flex items-center gap-x-2 pt-3 pb-0">
+                  <IconBadge icon={Eye} />
+                  <h2 className="text-xl">Access Settings</h2>
+                </div>
+                <ChapterAccessForm
+                  initialData={chapter}
+                  courseId={params.courseId}
+                  chapterId={params.chapterId}
+                />
+              </div>
             </div>
           </div>
-        </div>
-        <div>
-          <div className="flex items-center gap-x-2">
-            <IconBadge icon={Video} />
-            <h2 className="text">Add a video</h2>
+          <div>
+            <div className="flex items-center gap-x-2">
+              <IconBadge icon={Video} />
+              <h2 className="text">Add a video</h2>
+            </div>
+            <ChapterVideoForm
+              initialData={chapter}
+              chapterId={params.chapterId}
+              courseId={params.courseId}
+            />
           </div>
-          <ChapterVideoForm
-            initialData={chapter}
-            chapterId={params.chapterId}
-            courseId={params.courseId}
-          />
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
--- components/icon-badge.tsx
+++ components/icon-badge.tsx
@@ -3,27 +3,30 @@ import { cva, type VariantProps } from 'class-variance-authority';
 
 import { cn } from '@/lib/utils';
 
-const backgroundVariants = cva('rounded-full flex items-center justify-center', {
-  variants: {
-    variant: {
-      default: 'bg-sky-100',
-      success: 'bg-emerald-100',
+const backgroundVariants = cva(
+  'rounded-full flex items-center justify-center',
+  {
+    variants: {
+      variant: {
+        default: 'bg-slate-200',
+        success: 'bg-emerald-100',
+      },
+      size: {
+        default: 'p-2',
+        sm: 'p-1',
+      },
     },
-    size: {
-      default: 'p-2',
-      sm: 'p-1',
+    defaultVariants: {
+      variant: 'default',
+      size: 'default',
     },
   },
-  defaultVariants: {
-    variant: 'default',
-    size: 'default',
-  },
-});
+);
 
 const iconVariants = cva('', {
   variants: {
     variant: {
-      default: 'text-sky-700',
+      default: 'text-slate-950',
       success: 'text-emerald-700',
     },
     size: {
@@ -37,14 +40,22 @@ const iconVariants = cva('', {
   },
 });
 
-type BackgroundVariantsProps = VariantProps<typeof backgroundVariants>;
+type BackgroundVariantsProps = VariantProps<
+  typeof backgroundVariants
+>;
 type IconVariantsProps = VariantProps<typeof iconVariants>;
 
-interface IconBadgeProps extends BackgroundVariantsProps, IconVariantsProps {
+interface IconBadgeProps
+  extends BackgroundVariantsProps,
+    IconVariantsProps {
   icon: LucideIcon;
 }
 
-export const IconBadge = ({ icon: Icon, variant, size }: IconBadgeProps) => {
+export const IconBadge = ({
+  icon: Icon,
+  variant,
+  size,
+}: IconBadgeProps) => {
   return (
     <div className={cn(backgroundVariants({ variant, size }))}>
       <Icon className={cn(iconVariants({ variant, size }))} />
