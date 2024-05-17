0359c0e868f1a4bd84667a1ec9c67df96f3c61b4
Cao Viet Anh <caovietanhbk@gmail.com>
2024-05-18 02:56:13 +0700

    Add course list to search

--- app/(dashboard)/(routes)/search/page.tsx
+++ app/(dashboard)/(routes)/search/page.tsx
@@ -1,10 +1,32 @@
 import { getCategories } from '@/data/categories';
 import { Categories } from './_components/categories';
+import { CoursesList } from '@/components/ui/search/courses-list';
 import { SearchInput } from '@/components/layout/search-input';
 import { Suspense } from 'react';
+import { getCourses } from '@/actions/get-courses';
+import { currentUserId } from '@/lib/auth';
+import { redirect } from 'next/navigation';
 
-const SearchPage = async () => {
+interface SearchPageProps {
+  searchParams: {
+    title: string;
+    categoryId: string;
+  };
+}
+
+const SearchPage = async ({ searchParams }: SearchPageProps) => {
+  const userId = await currentUserId();
+
+  if (!userId) {
+    return redirect('/');
+  }
+
   const categories = await getCategories();
+
+  const courses = await getCourses({
+    userId,
+    ...searchParams,
+  });
 
   return (
     <>
@@ -13,8 +35,9 @@ const SearchPage = async () => {
           <SearchInput />
         </Suspense>
       </div>
-      <div className="p-6">
+      <div className="p-6 space-y-4">
         <Categories items={categories} />
+        <CoursesList items={courses} />
       </div>
     </>
   );
