a9c7da3274ab92d95108a9f6bef16df30ceda0db
Cao Việt Anh <131228985+vanhcao3@users.noreply.github.com>
2024-05-19 23:58:50 +0700

    Role management (#20)
    * Fix layout sidebar and content flexbox issue
    
    * Add role gate
    
    * Add user settings page
    
    * Add profile page
    
    * Add login and register button modal

--- components/ui/auth/register-button.tsx
+++ components/ui/auth/register-button.tsx
@@ -3,6 +3,13 @@ import React from 'react';
 
 import { useRouter } from 'next/navigation';
 
+import {
+  Dialog,
+  DialogContent,
+  DialogTrigger,
+} from '@/components/ui/dialog'
+
+import { RegisterForm } from './register-form'
 interface LoginButtonProps {
   children: React.ReactNode;
   mode?: 'modal' | 'redirect';
@@ -17,7 +24,16 @@ export const RegisterButton = ({ children, mode = 'redirect', asChild }: LoginBu
   };
 
   if (mode === 'modal') {
-    return <span></span>;
+    return (
+      <Dialog>
+        <DialogTrigger asChild={asChild}>
+          {children}
+        </DialogTrigger>
+        <DialogContent className="p-0 w-auto bg-transparent border-none">
+          <RegisterForm />
+        </DialogContent>
+      </Dialog>
+    )
   }
 
   return (
