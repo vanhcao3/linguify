733d1d34e56d1cbd4aaae4446665e539fe3c04ae
TheCoTam <hoangnguyenanhtuyen@gmail.com>
2024-05-20 21:30:12 +0700

    hoàn thiện nút đọc tất cả thông báo

--- components/Me/NotificationHeader.tsx
+++ components/Me/NotificationHeader.tsx
@@ -1,12 +1,22 @@
 'use client';
+import toast from 'react-hot-toast';
+import { useRouter } from 'next/navigation';
 
 import styles from '@/styles/Me/Notification.module.css';
+import { readAllNotifications } from '@/actions/notifications';
 
 function Header() {
+  const router = useRouter();
+
+  const handleClick = () => {
+    readAllNotifications();
+    toast.success('Đánh dấu thành công!');
+    router.refresh();
+  };
   return (
     <div className={styles['header']}>
       <div className={styles['header-title']}>Thông báo</div>
-      <div className={styles['header-button']}>
+      <div className={styles['header-button']} onClick={handleClick}>
         Đánh dấu là đã đọc
       </div>
     </div>
